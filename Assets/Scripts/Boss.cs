49371b79e3dc6fe6ad13af36ebf8d02fa6e3e508
GorayaU <144922343+GorayaU@users.noreply.github.com>
2023-12-02 00:50:15 -0500

    updated aim + shooting timer

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,62 +1,57 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    [SerializeField] private GameObject Player; // The GameObject whose trajectory you want to calculate
-    [SerializeField] private GameObject BulletPrefab; // The GameObject you want to launch
+    [SerializeField] private GameObject targetObject; // The GameObject whose trajectory you want to calculate
+    [SerializeField] private GameObject objectPrefab; // The GameObject you want to launch
+    [SerializeField] private float launchDelay = 2f; // The delay before launching the object
     [SerializeField] private float launchSpeed = 10f; // The speed at which the object will be launched
 
-    private Rigidbody2D targetRigidbody;
-    private Vector3 NewPos;
+    public bool inBossFight = false; // Variable to control if the boss is in a fight
 
-    public bool inBossFight;
+    private float timer;
 
-    private void Start()
+    private void Update()
     {
-        targetRigidbody = Player.GetComponent<Rigidbody2D>();
-        NewPos = Vector3.right / 5;
+        timer += Time.deltaTime;
+        if (timer >= launchDelay)
+        {
+            LaunchObjectDelayed();
+            timer = 0;
+        }
     }
-
-    private void FixedUpdate()
+    private void LaunchObjectDelayed()
     {
         if (inBossFight)
         {
-            // Get the player's position and velocity
-            Vector3 playerPosition = Player.transform.position;
-            Vector3 playerVelocity = targetRigidbody.velocity;
+            Vector2 targetVelocity = new Vector2(targetObject.GetComponent<Rigidbody2D>().velocity.x, math.clamp(-targetObject.GetComponent<Rigidbody2D>().velocity.y, -5, 1));
 
-            // Calculate the predicted position of the player in future time
-            Vector3 predictedPosition = PredictTargetPosition(playerPosition, playerVelocity, BulletPrefab.transform.position, launchSpeed);
+            Debug.Log(targetVelocity);
 
-            // Keep the Y position of the player but track the X position
-            Vector3 trackedPosition = new Vector3(predictedPosition.x, predictedPosition.y, 0);
-
-            ShootBullet(trackedPosition);
+            Vector3 predictedPosition = PredictTargetPosition(targetObject.transform.position, targetVelocity , objectPrefab.transform.position, launchSpeed);
+            LaunchObject(predictedPosition);
         }
     }
 
-    private Vector3 PredictTargetPosition(Vector3 targetPosition, Vector3 targetVelocity, Vector3 launchPosition, float launchSpeed)
+    private Vector2 PredictTargetPosition(Vector2 targetPosition, Vector2 targetVelocity, Vector2 launchPosition, float launchSpeed)
     {
-        // Calculate the distance to the target
-        Vector3 targetOffset = targetPosition - launchPosition;
+        Vector2 targetOffset = targetPosition - launchPosition;
         float time = targetOffset.magnitude / launchSpeed;
 
-        // Calculate the predicted position using the target's velocity
-        Vector3 predictedPosition = targetPosition + targetVelocity * time;
+        Vector2 predictedPosition = targetPosition + targetVelocity * time;
 
         return predictedPosition;
     }
 
-    private void ShootBullet(Vector3 targetPosition)
+    private void LaunchObject(Vector3 targetPosition)
     {
-        // Calculate the direction towards the predicted position of the player
-        Vector3 launchDirection = (targetPosition - transform.position).normalized;
+        Vector2 launchDirection = (targetPosition - transform.position).normalized;
 
-        GameObject Bullet = Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+        GameObject launchedObject = Instantiate(objectPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D rb = launchedObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(launchDirection * launchSpeed, ForceMode2D.Impulse);
 
-        Rigidbody2D bulletRigidbody = Bullet.GetComponent<Rigidbody2D>();
-
-        bulletRigidbody.AddForce(launchDirection * launchSpeed, ForceMode2D.Impulse);
-        Destroy(Bullet, 3);
+        Destroy(launchedObject, 3);
     }
 }
